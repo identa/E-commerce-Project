682e7dd1921f6f4dd290e5ffa61f5e4b2ae12135
tatrunganh1996 <tatrunganha28@gmail.com>
2019-02-20 10:15:31 +0700

    add service result

--- api/src/main/java/com/dac/spring/service/CustomerService.java
+++ api/src/main/java/com/dac/spring/service/CustomerService.java
@@ -1,4 +1,8 @@
 package com.dac.spring.service;
 
+import com.dac.spring.model.CustomerSignInRequest;
+import com.dac.spring.model.ServiceResult;
+
 public interface CustomerService {
-}
+
+    }
--- api/src/main/java/com/dac/spring/service/impl/CustomerServiceImpl.java
+++ api/src/main/java/com/dac/spring/service/impl/CustomerServiceImpl.java
@@ -1,5 +1,7 @@
 package com.dac.spring.service.impl;
 
+import com.dac.spring.model.CustomerSignInRequest;
+import com.dac.spring.model.ServiceResult;
 import com.dac.spring.service.CustomerService;
 
 public class CustomerServiceImpl implements CustomerService {
