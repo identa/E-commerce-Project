3f221c95d245f0771b016900ad23f232df559dbc
tatrunganh1996 <tatrunganha28@gmail.com>
2019-02-20 09:34:29 +0700

    add order repository

---
+++ api/src/main/java/com/dac/spring/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.boot_demo.repository;
+
+import com.example.boot_demo.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
+}
