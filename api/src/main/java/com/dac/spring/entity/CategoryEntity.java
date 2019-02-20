36ee8d7d11ac9bf1489663a3420bd9a86887e16f
tatrunganh1996 <tatrunganha28@gmail.com>
2019-02-20 09:29:21 +0700

    add category entity

---
+++ api/src/main/java/com/dac/spring/entity/CategoryEntity.java
@@ -0,0 +1,54 @@
+package com.example.boot_demo.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int parentID;
+
+    @OneToMany(mappedBy = "category")
+    private List<ProductEntity> productEntityList;
+
+    public CategoryEntity() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getParentID() {
+        return parentID;
+    }
+
+    public void setParentID(int parentID) {
+        this.parentID = parentID;
+    }
+
+    public List<ProductEntity> getProductEntityList() {
+        return productEntityList;
+    }
+
+    public void setProductEntityList(List<ProductEntity> productEntityList) {
+        this.productEntityList = productEntityList;
+    }
+}
