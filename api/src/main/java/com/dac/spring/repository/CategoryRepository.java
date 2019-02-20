8aebeec81d79f635df6c051d9bf9143c9c63005e
tatrunganh1996 <tatrunganha28@gmail.com>
2019-02-20 09:31:33 +0700

    add category repository

---
+++ api/src/main/java/com/dac/spring/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.boot_demo.repository;
+
+import com.example.boot_demo.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+}
