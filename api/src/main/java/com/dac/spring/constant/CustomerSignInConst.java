d13fb1c8bcdd6e7566de0995496d4e264d2b88c9
tatrunganh1996 <tatrunganha28@gmail.com>
2019-02-23 18:18:37 +0700

    add customer signin const class

---
+++ api/src/main/java/com/dac/spring/constant/CustomerSignInConst.java
@@ -0,0 +1,7 @@
+package com.dac.spring.constant;
+
+public class CustomerSignInConst {
+    public static final String SUCCESS = "You signed in successfully";
+    public static final String EMAIL_NOT_FOUND = "This account was not signed up";
+    public static final String EMAIL_PASSWORD_WRONG_FORMAT = "Email or password is not correct";
+}
